628ca00ff4fdbcd5f2c652be351f0690e660e0c1
Alan <alopez339@cps.edu>
2023-09-11 10:49:47 -0500

    change

--- a1.py
+++ a1.py
@@ -65,12 +65,11 @@ def every_other(lst: List[T]) -> List[T]:
     Returns:
         a list of every of other item in the original list starting with the first
     """
-
-    List[T] = lst
-    for el in lst:
-        if lst.index(el) / 2 == 0
-            lst.remove(el)
     
+    for el in List[T]:
+        if lst.index(el) % 2 == 0:
+            lst.remove(el)
+    return T
 
 
 
@@ -141,11 +140,7 @@ if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
     assert absolute(1) == 1, "absolute of -1 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
-    assert every_other([1, 2, 3, 4, 5]) == [
-        1,
-        3,
-        5,
-    ], "every_other of [1,2,3,4,5] failed"
+    assert every_other([1, 2, 3, 4, 5]) == [1, 3, 5], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
